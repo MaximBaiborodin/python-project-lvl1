ba8cf4036bf7b30fe860ad4b8ef6dcef7ab0094a
Maxim Baiborodin <mbaiborodin1@gmail.com>
2022-08-18 22:19:29 +0300

    add even_game script

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import prompt
+
+from random import randint
+
+
+def main():
+    even_game()
+
+
+def even_game():
+    print("Welcome to the Brain Games!")
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}')
+    print("Answer 'yes' if the number is even, otherwise answer 'no'.")
+    i = 0
+    while i < 3:
+        question = randint(1, 100)
+        print(f'Question: {question}')
+        answer = prompt.string('Your answer: ')
+        if question % 2 == 0 and answer == 'yes':
+            print('Correct!')
+            i += 1
+        elif question % 2 != 0 and answer == 'no':
+            print('Correct!')
+            i += 1
+        elif question % 2 == 0 and answer != 'yes':
+            print(f"{answer} is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, {name}")
+            return
+        elif question % 2 != 0 and answer != 'no':
+            print(f"{answer} is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {name}")
+            return
+    print(f'Congratulations, {name}!')
+
+
+if __name__ == '__main__':
+    main()
