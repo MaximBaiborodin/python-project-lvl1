4b692af4091084ed6ffa5b52210909923732430d
Maxim Baiborodin <mbaiborodin1@gmail.com>
2022-09-01 16:32:39 +0300

    add calc_game

--- brain_games/games/is_even.py
+++ brain_games/games/is_even.py
@@ -5,10 +5,6 @@ import prompt
 import random
 
 
-def main():
-    even_game()
-
-
 def even_game():
     print("Welcome to the Brain Games!")
     name = prompt.string('May I have your name? ')
@@ -16,21 +12,21 @@ def even_game():
     print("Answer 'yes' if the number is even, otherwise answer 'no'.")
     score = 0
     while score < 3:
-        answer = random.randint(1, 100)
-        print(f'Question: {answer}')
-        usr_answ = prompt.string('Your answer: ')
-        if answer % 2 == 0 and usr_answ == 'yes':
+        question = random.randint(1, 100)
+        print(f'Question: {question}')
+        answer = prompt.string('Your answer: ')
+        if question % 2 == 0 and answer == 'yes':
             print('Correct!')
             score += 1
-        elif answer % 2 != 0 and usr_answ == 'no':
+        elif question % 2 != 0 and answer == 'no':
             print('Correct!')
             score += 1
-        elif answer % 2 == 0 and usr_answ != 'yes':
-            print(f"{usr_answ} is wrong answer ;(. Correct answer was 'yes'.\
+        elif question % 2 == 0 and answer != 'yes':
+            print(f"{answer} is wrong answer ;(. Correct answer was 'yes'.\
 Let's try again, {name}")
             return
-        elif answer % 2 != 0 and usr_answ != 'no':
-            print(f"{usr_answ} is wrong answer ;(. Correct answer was 'no'.\
+        elif question % 2 != 0 and answer != 'no':
+            print(f"{answer} is wrong answer ;(. Correct answer was 'no'.\
 Let's try again, {name}")
             return
     print(f'Congratulations, {name}!')
