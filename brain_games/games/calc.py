c19df7fb50bfc4b6023690e1521a609948574dc4
Maxim Baiborodin <mbaiborodin1@gmail.com>
2022-09-13 23:54:52 +0300

    fixing small nistakes((((

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -11,7 +11,7 @@ def get_question_and_answer():
     num1 = random.randint(1, 10)
     num2 = random.randint(1, 10)
     op = random.choice(operator)
-    question = f'{num1}{op}{num2}'
+    question = f'{num1} {op} {num2}'
     if op == '+':
         answer = str(num1 + num2)
     if op == '-':
