481205d93f9147534e283ca466055003ba08689d
Maxim Baiborodin <mbaiborodin1@gmail.com>
2022-09-14 00:24:16 +0300

    fixing small mistakes((((

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -7,11 +7,13 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def get_question_and_answer():
-    num = random.randint(1, 50)
+    num = random.randint(1, 5)
     question = num
     answer = 'yes'
     for i in range(2, num):
         if num % i == 0:
             answer = 'no'
             break
+    if num == 1:
+        answer = 'no'
     return question, answer
