1d97591e5bf59e3adbda358949df3ec4824af5f2
Maxim Baiborodin <mbaiborodin1@gmail.com>
2022-09-19 23:00:53 +0300

    refactoring prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -6,14 +6,19 @@ import random
 DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
+def is_prime(question):
+    if question == 1:
+        return False
+    for i in range(2, question):
+        if question % i == 0:
+            return False
+    return True
+
+
 def get_question_and_answer():
-    num = random.randint(1, 50)
-    question = num
-    answer = 'yes'
-    for i in range(2, num):
-        if num % i == 0:
-            answer = 'no'
-            break
-    if num == 1:
+    question = random.randint(1, 5)
+    if is_prime(question):
+        answer = 'yes'
+    else:
         answer = 'no'
     return question, answer
