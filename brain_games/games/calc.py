bdb5731b46b556b0d2974d84bdfcbf8f5e355c65
Maxim Baiborodin <mbaiborodin1@gmail.com>
2022-09-20 00:21:06 +0300

    refactoring calc game

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
 
 import random
-
+import operator
 
 DESCRIPTION = 'What is the result of the expression?'
 
 
 def get_question_and_answer():
-    operator = ['+', '-', '*']
+    operators = {
+        '+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul,
+    }
     num1 = random.randint(1, 10)
     num2 = random.randint(1, 10)
-    op = random.choice(operator)
+    op = random.choice(list(operators.keys()))
     question = f'{num1} {op} {num2}'
-    if op == '+':
-        answer = str(num1 + num2)
-    if op == '-':
-        answer = str(num1 - num2)
-    if op == '*':
-        answer = str(num1 * num2)
+    answer = str(operators.get(op)(num1, num2))
     return question, answer
